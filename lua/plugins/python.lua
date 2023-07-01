5d9f95a0bca55dee55ab9a7447eacdb8b4fdee47
Victor Tantillo <v.tantillo3@gmail.com>
2023-07-01 09:29:04 -0600

    Added configuration for Markdown and Python

---
+++ lua/plugins/python.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = "basic",
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                diagnosticMode = "workspace",
+              },
+            },
+          },
+        },
+        ruff_lsp = {},
+      },
+    },
+  },
+}
