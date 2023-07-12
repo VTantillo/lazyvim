cf0e57a5b3f05304e968759aa5772cbe7d648e78
Victor Tantillo <v.tantillo3@gmail.com>
2023-07-12 09:33:31 -0600

    Updated python config some
    Still want to figure out how to get it to run Ruff's import sorting automatically and run tests easily like I have for
    JS

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "ninja", "python", "rst", "toml" })
+      end
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
@@ -17,6 +25,29 @@ return {
         },
         ruff_lsp = {},
       },
+      setup = {
+        ruff_lsp = function()
+          require("lazyvim.util").on_attach(function(client, _)
+            if client.name == "ruff_lsp" then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end)
+        end,
+      },
     },
+  },
+  {
+    "linux-cultist/venv-selector.nvim",
+    cmd = "VenvSelect",
+    opts = {},
+    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.formatting.black)
+    end,
   },
 }
