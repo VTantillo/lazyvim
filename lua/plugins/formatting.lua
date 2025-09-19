8f1a3332af6b97e32ee2548d79b231c42305a082
Victor Tantillo <victor.tantillo@lument.com>
2025-09-19 09:16:41 -0600

    Updated better for some reason

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       table.insert(opts.ensure_installed, "prettierd")
     end,
@@ -11,18 +11,18 @@ return {
     opts = {
       formatters_by_ft = {
         ["python"] = { "ruff_format", "ruff_fix", "ruff_organize_imports" },
-        ["javascript"] = { "prettierd", "prettier", stop_after_first = true },
-        ["javascriptreact"] = { "prettierd", "prettier", stop_after_first = true },
-        ["typescript"] = { "prettierd", "prettier", stop_after_first = true },
-        ["typescriptreact"] = { "prettierd", "prettier", stop_after_first = true },
-        ["css"] = { "prettierd", "prettier", stop_after_first = true },
-        ["html"] = { "prettierd", "prettier", stop_after_first = true },
-        ["json"] = { "prettierd", "prettier", stop_after_first = true },
-        ["jsonc"] = { "prettierd", "prettier", stop_after_first = true },
-        ["yaml"] = { "prettierd", "prettier", stop_after_first = true },
-        ["markdown"] = { "prettierd", "prettier", stop_after_first = true },
-        ["markdown.mdx"] = { "prettierd", "prettier", stop_after_first = true },
-        ["graphql"] = { "prettierd", "prettier", stop_after_first = true },
+        ["javascript"] = { "prettier", "prettierd", stop_after_first = true },
+        ["javascriptreact"] = { "prettier", "prettierd", stop_after_first = true },
+        ["typescript"] = { "prettier", "prettierd", stop_after_first = true },
+        ["typescriptreact"] = { "prettier", "prettierd", stop_after_first = true },
+        ["css"] = { "prettier", "prettierd", stop_after_first = true },
+        ["html"] = { "prettier", "prettierd", stop_after_first = true },
+        ["json"] = { "prettier", "prettierd", stop_after_first = true },
+        ["jsonc"] = { "prettier", "prettierd", stop_after_first = true },
+        ["yaml"] = { "prettier", "prettierd", stop_after_first = true },
+        ["markdown"] = { "prettier", "prettierd", stop_after_first = true },
+        ["markdown.mdx"] = { "prettier", "prettierd", stop_after_first = true },
+        ["graphql"] = { "prettier", "prettierd", stop_after_first = true },
       },
     },
   },
